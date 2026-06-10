dd8bea885041086d0b3d767caeaf9c20b1ef482a
itsacrow123 <mycpatricks@gmail.com>
2026-06-10 20:45:01 +0530

    Added sitemap.xml

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,13 @@
+const sitemapPlugin = require("@quasibit/eleventy-plugin-sitemap");
+
 module.exports = function(eleventyConfig) {
+
+  eleventyConfig.addPlugin(sitemapPlugin, {
+    sitemap: {
+      hostname: "https://usapesthelp.us"
+    }
+  });
+
   return {
     dir: {
       input: "src",
